2a0f09afaeaf0ff758b5db87fb1e218b5efa2a14
uki <uros.3.stevanovic@gmail.com>
2022-06-12 22:24:15 +0200

    initial commit

---
+++ src/main/java/com/example/marioradi/Controller.java
@@ -0,0 +1,13 @@
+package com.example.marioradi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/example/marioradi/MarioradiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marioradi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioradiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

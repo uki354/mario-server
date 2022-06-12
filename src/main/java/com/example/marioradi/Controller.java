d1ae9651ed02eda3e9460ea4992f207ccfd0991b
uki <uros.3.stevanovic@gmail.com>
2022-06-12 22:29:20 +0200

    testing again

--- src/main/java/com/example/marioradi/Controller.java
+++ src/main/java/com/example/marioradi/Controller.java
@@ -1,14 +1,13 @@
 package com.example.marioradi;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping
     public String helloWorld(){
-        return "Hello World";
+        return "index.html";
     }
 }
- 
